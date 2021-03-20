7d107c4993bc5e4ea2aa784c0a6135ecbd068cbb
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-03-20 19:15:07 +0100

    Tests

--- src/tests/vector.cpp
+++ src/tests/vector.cpp
@@ -1,31 +1,71 @@
 #include "../../include/Geometry/Geometry.hpp"
 #include "../../include/tests/test.hpp"
 
-void IntOperatorPlus() {
-    gmt::Vector<float> lhs = gmt::Vector<float>(1, 2);
-    gmt::Vector<float> rhs = gmt::Vector<float>(1, 3);
+template <typename T>
+void OperatorPlus() {
+    gmt::Vector<T> lhs = gmt::Vector<T>(1, 2);
+    gmt::Vector<T> rhs = gmt::Vector<T>(1, 3);
 
-    gmt::Vector<float> res = gmt::Vector<float>(2, 5);
+    gmt::Vector<T> test_result = lhs + rhs;
+    gmt::Vector<T> true_result = gmt::Vector<T>(2, 5);
+    ASSERT_THROW(test_result == true_result);
 
-    ASSERT_THROW((lhs + rhs) == res);
-    ASSERT_THROW((rhs + lhs) == res);
-    ASSERT_THROW((rhs + rhs) != res);
+    test_result = rhs + lhs;
+    true_result = gmt::Vector<T>(2, 5);
+    ASSERT_THROW(test_result == true_result);
+
+    test_result = rhs + rhs;
+    true_result = gmt::Vector<T>(2, 6);
+    ASSERT_THROW(test_result == true_result);
 }
 
-void IntOperatorMinus() {
-    gmt::Vector<float> lhs = gmt::Vector<float>(1, 2);
-    gmt::Vector<float> rhs = gmt::Vector<float>(1, 3);
+template <typename T>
+void OperatorMinus() {
+    gmt::Vector<T> lhs = gmt::Vector<T>(1, 2);
+    gmt::Vector<T> rhs = gmt::Vector<T>(1, 3);
 
-    gmt::Vector<float> resA = gmt::Vector<float>(0, -1);
-    gmt::Vector<float> resB = gmt::Vector<float>(0, 1);
-    gmt::Vector<float> resC = gmt::Vector<float>(0, 0);
+    gmt::Vector<T> test_result = lhs - rhs;
+    gmt::Vector<T> true_result = gmt::Vector<T>(0, -1);
+    ASSERT_THROW(test_result == true_result);
 
-    ASSERT_THROW((lhs - rhs) == resA);
-    ASSERT_THROW((rhs - lhs) == resB);
-    ASSERT_THROW((rhs - rhs) == resC);
+    test_result = rhs - lhs;
+    true_result = gmt::Vector<T>(0, 1);
+    ASSERT_THROW(test_result == true_result);
+
+    test_result = rhs - rhs;
+    true_result = gmt::Vector<T>(0, 0);
+    ASSERT_THROW(test_result == true_result);
+}
+
+template <typename T>
+void FunctionDot() {
+    T test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(), gmt::Vector<T>());
+    T true_result = T(0);
+    ASSERT_EQUAL(test_result, true_result);
+
+    test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(15, 7), gmt::Vector<T>(4, 78));
+    true_result = T(606);
+    ASSERT_EQUAL(test_result, true_result);
+
+    test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(84, 14), gmt::Vector<T>(878, 1105));
+    true_result = T(89222);
+    ASSERT_EQUAL(test_result, true_result);
+
+    test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(-15, -45), gmt::Vector<T>(-12, 0));
+    true_result = T(180);
+    ASSERT_EQUAL(test_result, true_result);
 }
 
 int main(int, char**) {
-    IntOperatorPlus();
-    IntOperatorMinus();
+    OperatorPlus<int>();
+    OperatorPlus<float>();
+    OperatorPlus<double>();
+
+    OperatorMinus<int>();
+    OperatorMinus<float>();
+    OperatorMinus<double>();
+
+    FunctionDot<int>();
+    FunctionDot<float>();
+    FunctionDot<double>();
 }
