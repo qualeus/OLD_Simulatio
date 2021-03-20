262f7ed0676ab79581ec146ecf4a7e34b52270b8
LHOUTELLIER Maël <55429341+mlhoutel@users.noreply.github.com>
2021-03-20 19:42:54 +0100

    Equality tests

--- include/tests/test.hpp
+++ include/tests/test.hpp
@@ -1,8 +1,9 @@
 // clang-format off
 #include <stdexcept>
 #include <string>
+#include "../Geometry/Maths.hpp"
 
-#define ASSERT_THROW(condition) { if (!(condition)) { throw std::runtime_error(std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__)); } }
+#define ASSERT_THROW(condition) { if (!(condition)) { throw std::runtime_error(std::string("\n") + std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__)); } }
 
-#define ASSERT_EQUAL(x, y) { if ((x) != (y)) { throw std::runtime_error(std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__) + std::string(": ") + std::to_string((x)) + std::string(" != ") + std::to_string((y))); } }
+#define ASSERT_EQUAL(x, y) { if ((gmt::to_string(x)) != (gmt::to_string(y))) { throw std::runtime_error(std::string("\n") + std::string(__FILE__) + std::string(":") + std::to_string(__LINE__) + std::string(" in ") + std::string(__PRETTY_FUNCTION__) + std::string("\n[Assertion Failed]\nExpected ") + gmt::to_string(x) + std::string("\nResult ") + gmt::to_string(y)); } }
 // clang-format on
--- src/tests/vector.cpp
+++ src/tests/vector.cpp
@@ -8,15 +8,15 @@ void OperatorPlus() {
 
     gmt::Vector<T> test_result = lhs + rhs;
     gmt::Vector<T> true_result = gmt::Vector<T>(2, 5);
-    ASSERT_THROW(test_result == true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = rhs + lhs;
     true_result = gmt::Vector<T>(2, 5);
-    ASSERT_THROW(test_result == true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = rhs + rhs;
     true_result = gmt::Vector<T>(2, 6);
-    ASSERT_THROW(test_result == true_result);
+    ASSERT_EQUAL(true_result, test_result);
 }
 
 template <typename T>
@@ -26,34 +26,41 @@ void OperatorMinus() {
 
     gmt::Vector<T> test_result = lhs - rhs;
     gmt::Vector<T> true_result = gmt::Vector<T>(0, -1);
-    ASSERT_THROW(test_result == true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = rhs - lhs;
     true_result = gmt::Vector<T>(0, 1);
-    ASSERT_THROW(test_result == true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = rhs - rhs;
     true_result = gmt::Vector<T>(0, 0);
-    ASSERT_THROW(test_result == true_result);
+    ASSERT_EQUAL(true_result, test_result);
 }
 
 template <typename T>
 void FunctionDot() {
     T test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(), gmt::Vector<T>());
     T true_result = T(0);
-    ASSERT_EQUAL(test_result, true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(15, 7), gmt::Vector<T>(4, 78));
     true_result = T(606);
-    ASSERT_EQUAL(test_result, true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(84, 14), gmt::Vector<T>(878, 1105));
     true_result = T(89222);
-    ASSERT_EQUAL(test_result, true_result);
+    ASSERT_EQUAL(true_result, test_result);
 
     test_result = gmt::Vector<T>::Dot(gmt::Vector<T>(-15, -45), gmt::Vector<T>(-12, 0));
     true_result = T(180);
-    ASSERT_EQUAL(test_result, true_result);
+    ASSERT_EQUAL(true_result, test_result);
+}
+
+template <typename T>
+void FunctionSegmentProjection() {
+    gmt::Vector<T> test_result = gmt::Vector<T>::SegmentProjection(gmt::Vector<T>(4, 3), gmt::Vector<T>(0, 0), gmt::Vector<T>(4, 0));
+    gmt::Vector<T> true_result = gmt::Vector<T>(4, 0);
+    ASSERT_EQUAL(true_result, test_result);
 }
 
 int main(int, char**) {
@@ -68,4 +75,8 @@ int main(int, char**) {
     FunctionDot<int>();
     FunctionDot<float>();
     FunctionDot<double>();
+
+    FunctionSegmentProjection<int>();
+    FunctionSegmentProjection<float>();
+    FunctionSegmentProjection<double>();
 }
