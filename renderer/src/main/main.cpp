77b87cb85a753882c9d5b8413966ab13a2108465
Maël <mlhoutel@gmail.com>
2021-12-05 15:28:33 +0100

    fix(Wasm): :zap: Update wasm setup

--- renderer/src/main/main.cpp
+++ renderer/src/main/main.cpp
@@ -1,29 +1,47 @@
 
 #include "../../include/Drawing/Window.hpp"
 
-int main(void) {
-    drw::Window window = drw::Window();
-    window.Render();
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
 
-    bool show_demo_window = true;
+drw::Window window;
+bool show_demo_window = true;
+
+void loop() {
+    window.Clear();
+
+    /*
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glVertex2f(0.0, 0.5);
+    glEnd();
+    */
+
+    if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+
+    /* Swap front and back buffers */
+    window.Draw();
+}
+
+int main(void) {
+    window = drw::Window();
+    window.Render();
+    show_demo_window = true;
+
+#ifdef __EMSCRIPTEN__
+    // EM_JS(int, canvas_get_width, (), { return Module.canvas.width; });
+    //  EM_JS(int, canvas_get_height, (), { return Module.canvas.height; });
+    //  EM_JS(void, resizeCanvas, (), { js_resizeCanvas(); });
+#endif
 
     /* Loop until the user closes the window */
-    while (window.isOpen()) {
-        window.Clear();
-
-        /*
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0.5, -0.5);
-        glVertex2f(0.0, 0.5);
-        glEnd();
-        */
-
-        if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
-
-        /* Swap front and back buffers */
-        window.Draw();
-    }
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(&loop, 0, 1);
+#else
+    while (window.isOpen()) { loop(); }
+#endif
 
     window.Close();
     return 0;
